9a163f45fe744822563b3ea4409984302a4cad72
Edward Robinson <proteenx11@gmail.com>
2015-06-17 13:55:32 +1200

    add mock for getAllUsers

--- ldap.development.js
+++ ldap.development.js
@@ -4,10 +4,24 @@ module.exports = function (config) {
   userGroups = config.mockUserToGroup;
 
   return {
-    getUserGroups: mockGetUserGroups
+    getUserGroups: mockGetUserGroups,
+    getAllUsers: mockGetAllUsers
   }
 }
 
 function mockGetUserGroups(username, callback) {
   callback(null, [userGroups[username]])
+}
+
+function mockGetAllUsers(callback) {
+  callback(null, [
+      {
+        id: 'jtaylor',
+        name: 'James Taylor'
+      },
+      {
+        id: 'agrant',
+        name: 'Alex Grant'
+      }
+    ])
 }
