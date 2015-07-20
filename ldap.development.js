ae57a119e977b4165a734a955571670f847cba9a
Edward Robinson <proteenx11@gmail.com>
2015-07-20 13:35:14 +1200

    use es6 arrow functions

--- ldap.development.js
+++ ldap.development.js
@@ -1,20 +1,4 @@
-var userGroups;
-
-module.exports = function (config) {
-  userGroups = config.mockUserToGroup;
-
-  return {
-    getUserGroups: mockGetUserGroups,
-    getAllUsers: mockGetAllUsers,
-    getUserEmail: mockGetUserEmail,
-    getUserName: mockGetUserName
-  }
-}
-
-function mockGetUserGroups(username, callback) {
-  callback(null, [userGroups[username]])
-}
-
+var userGroups
 var users = [
     {
       sAMAccountName: 'taytay',
@@ -25,17 +9,15 @@ var users = [
       cn: 'Alex Grant'
     }
 ]
-
-function mockGetAllUsers(callback) {
-  callback(null, users)
-}
-
-function mockGetUserEmail(username, callback) {
-  callback(null, 'developers@mediasuite.co.nz')
-}
-
 var userByName = { 'taytay': users[0], 'agrant': users[1] }
 
-function mockGetUserName(id, callback) {
-  callback(null, usersByName[id])
+module.exports = function (config) {
+  userGroups = config.mockUserToGroup
+
+  return {
+    getUserGroups: (username, callback) => callback(null, [userGroups[username]]),
+    getAllUsers: callback => callback(null, users),
+    getUserEmail: (username, callback) => callback(null, 'developers@mediasuite.co.nz'),
+    getUserName: (id, callback) => callback(null, usersByName[id])
+  }
 }
