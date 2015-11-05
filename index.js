bcae623dd54d0bfc1630a71375faffc221a384df
Richard Walker <digitalsadhu@gmail.com>
2015-11-05 15:54:16 +0100

    Fix standard errors

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
-'use strict';
+'use strict'
 
-var ldap;
-var ldapConfig;
+var ldap
+var ldapConfig
 
-function getUserInfo(req, res, next) {
-  var username;
-  if (ldapConfig.mock)
-    username = ldapConfig.mockUsername;
-  else
-    username = req.headers['x-iisnode-auth_user'].split('\\')[1];
-
+function getUserInfo (req, res, next) {
+  var username
+  if (ldapConfig.mock) {
+    username = ldapConfig.mockUsername
+  } else {
+    username = req.headers['x-iisnode-auth_user'].split('\\')[1]
+  }
   ldap.getUserGroups(username, function (err, groups) {
-
-    if (err)
-      return next(err);
-
-    if (!groups[0])
+    if (err) {
+      return next(err)
+    }
+    if (!groups[0]) {
       return next(new Error(
         username + ' has not been given access to this app'
-      ));
-
+      ))
+    }
     req.user = {
       name: username,
       group: groups[0]
-    };
+    }
 
-    next();
-  });
+    next()
+  })
 }
 
 module.exports = function (config) {
-  if (!config)
-    console.error('\nldapconfig.json is missing! Please copy ldapconfig-example.json in to your server/ directory and make the appropriate changes.');
-
-  ldapConfig = config;
-  var ldapLibrary = ldapConfig.mock ?
-    require('./ldap.development') :
-    require('./ldap.production');
-  ldap = ldapLibrary(ldapConfig);
+  if (!config) {
+    console.error('\nldapconfig.json is missing! Please copy ldapconfig-example.json in to your server/ directory and make the appropriate changes.')
+  }
+  ldapConfig = config
+  var ldapLibrary = ldapConfig.mock ? require('./ldap.development') : require('./ldap.production')
+  ldap = ldapLibrary(ldapConfig)
 
   return {
     getUserInfo: getUserInfo
-  };
-};
+  }
+}
